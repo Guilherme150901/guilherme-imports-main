d5ffc9e4b7687dbd6c259bb166d269d60a932208
GUilherme Andrade <gui15092001@gmail.com>
2025-01-12 11:46:43 -0300

    finalizando o projeto

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+//TODO: Toda rota que voce criar no app, vc tem que referenciar aqui no código da vite.config.js
 export default {
     build: {
       rollupOptions: {
         input: {
           main: './index.html',
           checkout: './checkout.html',
+          //Example:
+          pedidos: './pedidos.html'
         },
       },
     },
